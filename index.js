90b73c5d251bf9c09716c4dad2be3ca697c63fc8
jencekade <gaephuoc@gmail.com>
2017-12-11 08:38:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtracts(a, b) {
+  return b - a;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increments(n) {
+  return n++;
+}
+
+function decrements(n) {
+  return n--;
+}
+
+function parses(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
